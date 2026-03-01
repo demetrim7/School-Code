3c301ee1040a0e593682ac05b73d0a2fa1ce127d
Demetri Martinez <demetrimartinez778@icloud.com>
2026-03-01 13:59:26 -0700

    exercise 1

---
+++ Project 2 - Exercise 1/index.js
@@ -0,0 +1,13 @@
+let partyLocation = "Theatre";
+const letterSwitch = 3;
+let mixedWord = "";
+let solvedWord = "";
+let isPartySafe = false;
+
+console.log(letterSwitch);
+
+const isInteger = Number.isInteger(letterSwitch);
+
+console.log(isInteger);
+
+console.log("hello?");
